e3e6c21a326e3fd0d035d486234a1b5890ddada8
BKM TG <119480777+MrBKM@users.noreply.github.com>
2022-12-11 09:29:52 +0530

    Update scripts.py

--- Midukki/scripts.py
+++ Midukki/scripts.py
@@ -11,13 +11,10 @@ HELP_TXT = """<i><b>Hello 👋 {mention},</i></b>
 
 <i><b>I can Guide You Through All Of {bot}'s Cool Features & How To Properly Use Them. Use The Buttons Below To Navigate Through All Of The Modules.</i></b>
 
-ABOUT_TXT = """<b>{name} Was created on December 8, 2022
-
-➾ Developers : Mr.BKM
-➾ Language : Python3
-➾ Framework : Pyrogram
-➾ Database : MongoDB
-</b>"""
+ABOUT_TXT = """Developers: Mr.BKM
+Language: Python3
+Framework: Pyrogram
+Database: MongoDB"""
 
 DONATE_TXT = """<i><b>Hello 👋 {mention}</i></b>
 
