56bf4681eb3c0dcd7a3d05ff9586ff9a2c0b4cd6
BKM TG <119480777+MrBKM@users.noreply.github.com>
2022-12-11 09:45:54 +0530

    Update scripts.py

--- Midukki/scripts.py
+++ Midukki/scripts.py
@@ -15,7 +15,7 @@ HELP_TXT = """<i><b>Hello 👋 {mention},</i></b>
 <i><b>I can Guide You Through All Of {bot}'s Cool Features & How To Properly Use Them. Use The Buttons Below To Navigate Through All Of The Modules.</i></b>
 
 SEND_LOGS_A = """
-🤖 Bot Started Successfully ☑️
+Bot Started Successfully
 """ 
 
 class Txt(object):
